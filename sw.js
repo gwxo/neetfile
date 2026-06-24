047f7a52a744298eda9a42d31e499cfb71364dd6
Gw team <codewithprashant07@gmail.com>
2026-06-24 09:49:52 +0530

    Fix asset paths in service worker

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'dr-prashant-v1';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
+  '/',
+  '/index.html',
+  '/manifest.json'
 ];
 
 // Install Service Worker
